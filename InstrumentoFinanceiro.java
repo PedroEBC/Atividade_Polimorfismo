6dfb80daa01857750db10ce26a4812b7750cccba
Pedro Estevam <116644843+PedroEBC@users.noreply.github.com>
2023-07-19 18:37:17 -0300

    Create InstrumentoFinanceiro.java

---
+++ InstrumentoFinanceiro.java
@@ -0,0 +1,26 @@
+package atividade;
+
+public abstract class InstrumentoFinanceiro {
+
+	private float saldo;
+
+	
+	public InstrumentoFinanceiro()
+	{
+		
+	}
+	
+	
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+	public float calcularSaldoTotal()
+	{
+		return this.saldo;
+	}
+}
